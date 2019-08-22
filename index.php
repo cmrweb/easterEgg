835fe6e537e3c5b82b08554af7e4511a581c41e9
cmrwebdev <33638879+cmrweb@users.noreply.github.com>
2019-08-22 23:10:05 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,47 @@
 <script>
-var key="";
-document.addEventListener('keydown', (event) => {
-  const inputName = event.key;
-  console.log(inputName)
-  if (inputName === 'ArrowUp' && key==="") {
-    key+="ArrowUp";
-  }
-  if (inputName === 'l' && key==="ArrowUp") {
-    key+="l";
-  }
-  if (inputName === 'o' && key==="ArrowUpl") {
-    key+="o";
-  }
-  if (inputName === 'l'  && key==="ArrowUplo") {
-    key+="l"; 
-  }
-  if(key==="ArrowUplol"){
-      console.log("lol")
-  }
-  //console.log(key)
+var key = "";
+var show = false;
+
+function allTime(inputName) {
+    if(show===false){
+        if (inputName === 'ArrowUp' && key === "") {
+            key += "ArrowUp";
+        }
+        if (inputName === 'l' && key === "ArrowUp") {
+            key += "l";
+        }
+        if (inputName === 'o' && key === "ArrowUpl") {
+            key += "o";
+        }
+        if (inputName === 'l' && key === "ArrowUplo") {
+            key += "l";
+        }        
+    }
+}
+
+function oneTime(inputName) {
+    if(show===false){
+        if (inputName === 'ArrowUp') {
+            key += "ArrowUp";
+        }
+        if (inputName === 'l') {
+            key += "l";
+        }
+        if (inputName === 'o') {
+            key += "o";
+        }
+    }
+}
+
+document.addEventListener('keydown', (e) => {
+    const inputName = e.key;
+    //allTime(inputName)
+    oneTime(inputName)
+    if (key === "ArrowUplol" && show === false) {
+        show = true;
+        key=""
+        console.log("lol")
+    }
+    //console.log(key)
 })
 </script>
