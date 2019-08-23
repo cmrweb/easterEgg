3d1481fffb7981e90d0acbb06fa49c8875bdfb82
cmrwebdev <33638879+cmrweb@users.noreply.github.com>
2019-08-23 20:35:47 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,46 +2,25 @@
 var key = "";
 var show = false;
 
-function allTime(inputName) {
+function eastEgg(inputName) {
     if(show===false){
-        if (inputName === 'ArrowUp' && key === "") {
-            key += "ArrowUp";
-        }
-        if (inputName === 'l' && key === "ArrowUp") {
-            key += "l";
-        }
-        if (inputName === 'o' && key === "ArrowUpl") {
-            key += "o";
-        }
-        if (inputName === 'l' && key === "ArrowUplo") {
-            key += "l";
-        }        
+        key += inputName;
+		if (key === "ArrowDownArrowUp") {
+			show = true;
+			key=""
+			console.log('code 1')
+            //DO SOMETHING
+		} 
+		if (key === "ArrowUpArrowDown") {
+			show = true;
+			key=""
+			console.log('code 2')
+            //DO SOMETHING
+		} 
     }
 }
-
-function oneTime(inputName) {
-    if(show===false){
-        if (inputName === 'ArrowUp') {
-            key += "ArrowUp";
-        }
-        if (inputName === 'l') {
-            key += "l";
-        }
-        if (inputName === 'o') {
-            key += "o";
-        }
-    }
-}
-
 document.addEventListener('keydown', (e) => {
     const inputName = e.key;
-    //allTime(inputName)
-    oneTime(inputName)
-    if (key === "ArrowUplol" && show === false) {
-        show = true;
-        key=""
-        console.log("lol")
-    }
-    //console.log(key)
+    eastEgg(inputName)
 })
 </script>
