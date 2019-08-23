34616306f6704e7eee62cb15e4ee4cbbb01e6459
cmrwebdev <33638879+cmrweb@users.noreply.github.com>
2019-08-23 23:47:20 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,35 @@
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
+
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>EasterEgg</title>
-	<style>
-	#easterEgg{
-		display: none;
-		position: absolute;
-		top: 0;bottom: 0;left: 0;right:0;
-	}
-	</style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CmrEasterEgg</title>
+    <style>
+    #easterEgg {
+        display: none;
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+    }
+    #easterEgg p {
+        text-align: center;
+        font-size: 7em;
+        font-weight: bold;
+    }
+    </style>
 </head>
+
 <body>
-	<div id="easterEgg"></div>
+    <p>code 1: &darr; &uarr; &larr; &rarr;</p>
+    <p>code 2: &uarr; &darr; &larr; &rarr;</p>
+
+    <div id="easterEgg"></div>
 </body>
+
 </html>
 
 
@@ -24,38 +37,42 @@
 var key = "";
 var show = false;
 var block = document.querySelector('#easterEgg');
-var content = ""
-function eastEgg(inputName) {
+var content = ["<p>code 1</p>", "<p>code 2</p>"];
+
+function eastEgg(input) {
     if (show === false) {
-        key += inputName;
+        key += input;
         switch (key) {
-            case "ArrowDownArrowUp":
+            case "40383739":
                 show = true;
                 key = "";
+                //
                 console.log('code 1');
-				block.style.display = "block";
-				block.style.backgroundColor = "#000";
-				block.append("lol");
-                
+                block.style.display = "block";
+                block.style.backgroundColor = "#333";
+                block.innerHTML = content[0];
+
                 break;
-            case "ArrowUpArrowDown":
+            case "38403739":
                 show = true;
                 key = "";
+                //
                 console.log('code 2');
-				block.style.display = "block";
-				block.style.backgroundColor = "#ccc";
-				block.append("lol");
-                
+                block.style.display = "block";
+                block.style.backgroundColor = "#ccc";
+                block.innerHTML = content[1];
+
                 break;
 
             default:
-                //DO SOMETHING
                 break;
         }
     }
 }
 document.addEventListener('keydown', (e) => {
-    const inputName = e.key;
-    eastEgg(inputName);
+    const input = e.keyCode;
+    eastEgg(input);
+    //
+    console.log(e.keyCode);
 })
 </script>
